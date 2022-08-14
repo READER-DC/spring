ce772691538313dad9468098c8882219c8169ccf
Anatolii Sehal <sab198005@gmail.com>
2022-08-14 20:49:11 +0300

    first commit

---
+++ springhelloxml/src/main/java/ua/kyiv/sehal/springhelloxml/App.java
@@ -0,0 +1,13 @@
+package ua.kyiv.sehal.springhelloxml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
