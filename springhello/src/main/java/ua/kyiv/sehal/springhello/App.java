25b7a8191aa67dd0d037f284c5d576bba326ca57
Anatolii Sehal <sab198005@gmail.com>
2022-08-14 22:21:45 +0300

    commit

---
+++ springhello/src/main/java/ua/kyiv/sehal/springhello/App.java
@@ -0,0 +1,13 @@
+package ua.kyiv.sehal.springhello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
