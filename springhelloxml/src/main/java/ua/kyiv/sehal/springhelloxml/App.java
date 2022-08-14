e161c75e2b49e1b71dee76ab381bcf1bda97981b
Anatolii Sehal <sab198005@gmail.com>
2022-08-14 21:12:15 +0300

    configuration beans in xml

--- springhelloxml/src/main/java/ua/kyiv/sehal/springhelloxml/App.java
+++ springhelloxml/src/main/java/ua/kyiv/sehal/springhelloxml/App.java
@@ -1,13 +1,18 @@
 package ua.kyiv.sehal.springhelloxml;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ClassPathXmlApplicationContext context = 
+        		new ClassPathXmlApplicationContext("applicationContext.xml");
+        
+        PersonBean person = context.getBean("PersonBean", PersonBean.class);
+        System.out.printf("%s - %-4d", person.getName(), person.getAge());
+        
+        context.close();
     }
 }
